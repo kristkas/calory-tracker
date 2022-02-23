0e52a070c6895c708ab48ada995c8b58387f6e66
kristkas <kristin.kasuk@gmail.com>
2022-02-23 22:47:49 +0200

    Added total calories

--- app.js
+++ app.js
@@ -34,12 +34,23 @@ const ItemCtrl = (function(){
             }
             // calories to number
             calories = parseInt(calories);
-            // crreate new item
+            // create new item
             newItem = new Item(ID,name,calories);
             // add to items array
             data.items.push(newItem);
             // return new item
             return newItem
+        },
+        getTotalCalories: function (){
+          let total = 0;
+          // loop through items and add calories
+            data.items.forEach(function (item){
+                total = total + item.calories;
+            });
+            // set total calories in data structure
+            data.total = total;
+            // return total
+            terurn.data.total;
         },
         logData: function () {
             return data
@@ -55,7 +66,8 @@ const UICtrl = (function(){
         itemList: '#item-list',
         itemNameInput: '#item-name',
         itemCaloriesInput: '#item - calories',
-        addBtn: '.add-btn'
+        addBtn: '.add-btn',
+        totalCalories: 'total-calories'
 }
     return {
         populateItemList: function (items) {
@@ -98,6 +110,9 @@ const UICtrl = (function(){
         clearInput: function (){
             document.querySelector(UISelectors.itemNameInput).value = '';
             document.querySelector(UISelectors.itemCaloriesInput).value = '';
+        },
+        showTotalCalories: function (totalCalories){
+            document.querySelector(UISelectors.totalCalories).textContent = totalCalories;
         }
     }
 })();
@@ -120,6 +135,10 @@ const App = (function(ItemCtrl, UICtrl){
             const newItem = ItemCtrl.addItem(input.name, input.calories)
             // add item to UI items list
             UICtrl.addListItem(newItem)
+            // get total calories
+            const totalCalories = ItemCtrl.getTotalCalories();
+            // add total calories to UI
+            UICtrl.showTotalCalories(totalCalories);
             // clear fields
             UICtrl.clearInput();
         }
